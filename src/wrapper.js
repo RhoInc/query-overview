2d936ac00726412b08e6a54b07cf35855b0f0e05
jwildfire <jwildfire@gmail.com>
2016-06-10 09:41:46 -0400

    Fix repo name in package.json

--- src/wrapper.js
+++ src/wrapper.js
@@ -16,12 +16,12 @@ export default function queryOverview(element, settings){
 	//keep settings in sync with the data mappings
 	mergedSettings = syncSettings(mergedSettings);
 	
-	//keep control inputs in sync and create controls object (if needed)
+	//keep control inputs in sync and create controls object 
 	let syncedControlInputs = syncControlInputs(controlInputs, mergedSettings);
 	let controls = createControls(element, {location: 'top', inputs: syncedControlInputs});
 	
 	//create chart
-	let chart = createChart(element, mergedSettings); //add 3rd controls object as needed 
+	let chart = createChart(element, mergedSettings, controls); 
 	chart.on('init', onInit);
 	chart.on('layout', onLayout);
 	chart.on('datatransform', onDataTransform);
