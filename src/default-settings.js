a53467745e8e2c25ab9f09e70f947873884eed34
jwildfire <jwildfire@gmail.com>
2016-06-15 17:12:03 -0400

    Check for duplicates before pushing to settings object

--- src/default-settings.js
+++ src/default-settings.js
@@ -46,22 +46,26 @@ const settings = {
 
 // Replicate settings in multiple places in the settings object
 export function syncSettings(settings){
-	settings.y.column = settings.form_col;
-	settings.marks[0].per[0] = settings.form_col;
-	settings.marks[1].per[0] = settings.form_col;
-	settings.marks[0].tooltip = settings.status_col ? "["+settings.status_col+"] - $x queries" : "$x queries";
-	settings.groupBy[0] = "FormField" //hard-coded variable derived in .on("init")
-	settings.groupBy[1] = settings.form_col
-	if(settings.status_col){
-		settings.marks[0].split = settings.status_col
-		settings.color_by = settings.status_col
-		settings.groupBy.push(settings.status_col)
-	}
-	if(settings.filter_cols){
-		settings.filter_cols.forEach(function(d){
-			settings.groupBy.push(d)
-		})
-	}
+  settings.y.column = settings.form_col;
+  settings.marks[0].per[0] = settings.form_col;
+  settings.marks[1].per[0] = settings.form_col;
+  settings.marks[0].tooltip = settings.status_col ? "["+settings.status_col+"] - $x queries" : "$x queries";
+  settings.groupBy[0] = "FormField" //hard-coded variable derived in .on("init")
+  settings.groupBy[1] = settings.form_col
+  if(settings.status_col){
+    settings.marks[0].split = settings.status_col
+    settings.color_by = settings.status_col
+    if(settings.groupBy.indexOf(settings.status_col)==-1){
+      settings.groupBy.push(settings.status_col);
+    } 
+  }
+  if(settings.filter_cols){
+    settings.filter_cols.forEach(function(d){
+      if(settings.groupBy.indexOf(d)==-1){
+        settings.groupBy.push(d);
+      }   
+    })
+  }
     return settings;
 }
 
@@ -87,6 +91,17 @@ export const controlInputs = [
 
 // Map values from settings to control inputs
 export function syncControlInputs(controlInputs, settings){
+  function containsObject(obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+
+    return false;
+  }
+
   var formControl = controlInputs.filter(function(d){return d.label=="Form: "})[0] 
   formControl.value_col = settings.form_col; 
 
@@ -94,26 +109,30 @@ export function syncControlInputs(controlInputs, settings){
   yColControl.values = settings.groupBy; 
 
   if(settings.status_col){
-  	var statusControl = {
-  		type:"subsetter", 
-  		value_col:settings.status_col, 
-  		label: "Status: ",
-  		multiple:true
-  	}
-  	controlInputs.push(statusControl)
+    var statusControl = {
+      type:"subsetter", 
+      value_col:settings.status_col, 
+      label: "Status: ",
+      multiple:true
+    }
+    if(containsObject(statusControl,controlInputs)==false){
+      controlInputs.push(statusControl)  
+    }
+    
   }
 
   if(settings.filter_cols){
-	settings.filter_cols.forEach(function(d,i){
-  		var thisFilter = {
-  			type:"subsetter", 
-  			value_col:d, 
-  			multiple:true
-  		}
-  		thisFilter.label = settings.filter_labels[i] ? settings.filter_labels[i] : null
-  		console.log(thisFilter)
-  		controlInputs.push(thisFilter)
-  	})
+  settings.filter_cols.forEach(function(d,i){
+      var thisFilter = {
+        type:"subsetter", 
+        value_col:d, 
+        multiple:true
+      }
+      thisFilter.label = settings.filter_labels[i] ? settings.filter_labels[i] : null
+      if(containsObject(thisFilter,controlInputs)==false){
+        controlInputs.push(thisFilter)  
+      }
+    })
   }
   return controlInputs
 }
