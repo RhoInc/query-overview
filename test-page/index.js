ef8735ce0aade33d94053a3161022ad7a3fd07be
samussiah <samussiah@gmail.com>
2019-03-21 14:16:37 -0400

    update test page; still need to work out date range styling in firefox

--- test-page/index.js
+++ test-page/index.js
@@ -1,14 +1,14 @@
 d3.csv(
     'https://raw.githubusercontent.com/RhoInc/data-library/master/data/clinical-trials/data-cleaning/queries.csv',
-    function(error,data) {
-        if (error)
-            console.log(error);
-
-        var settings = {
-        };
+    //'../../data-library/data/clinical-trials/data-cleaning/queries.csv', // load local data file for better performance
+    function(d) {
+        return d;
+    },
+    function(data) {
         var instance = queryOverview(
-            '#container',
-            settings
+            '#container', // element
+            {
+            } // settings
         );
         instance.init(data);
     }
